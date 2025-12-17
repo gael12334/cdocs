17f62c719207b237837294f9ca75a815ff1ad59f
Gaël Fortier <fortier267@gmail.com>
2025-12-17 15:40:33 -0500

    Update readme + move example

---
+++ doc.sh
@@ -0,0 +1,9 @@
+#
+# Copyright © 2025 Gaël Fortier <gael.fortier.1@ens.etsmtl.ca>
+#
+if [ ! -d "./out" ]; then
+    mkdir ./out
+fi
+gcc -o ./out/program -Werror -Wreturn-type -ggdb $(find ./src -name '*.c' -print) -I/usr/include -L/usr/lib -static-libgcc
+chmod +x ./out/program
+cp ./src/main.c.md ./docs/docs.md
